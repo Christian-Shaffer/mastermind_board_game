eac4e5c5360bbfc1d6740a4ff649cf12dcd2c9aa
Christian Shaffer <calexandershaffer@gmail.com>
2023-11-22 10:58:01 -0500

    Add option to be the creator to have the computer make random guesses

--- app.rb
+++ app.rb
@@ -6,22 +6,6 @@ class Guesser
     @turns = 0
   end
 
-  def make_guess
-    @chosen_colors = []
-    puts 'Guess one at a a time. Start with 1, then 2, etc.'
-    while @chosen_colors.length < 4
-      guess = gets.chomp
-      if valid_guess?(guess)
-        @chosen_colors << guess
-      else
-        puts 'Invalid. Needs to be one of the six valid colors without spelling mistakes or repeats for each turn.'
-      end
-    end
-    puts "You guessed: #{@chosen_colors}"
-    @turns += 1
-    @chosen_colors
-  end
-
   def valid_guess?(guess)
     if !@chosen_colors.include?(guess) && Game.color_options.include?(guess)
       true
@@ -36,6 +20,37 @@ class Player < Guesser
     super
     puts 'hehe'
   end
+
+  def make_guess
+    @chosen_colors = []
+    puts 'Guess one at a a time. Start with 1, then 2, etc.'
+    while @chosen_colors.length < 4
+      guess = gets.chomp
+      if valid_guess?(guess)
+        @chosen_colors << guess
+      else
+        puts 'Invalid. Needs to be one of the six valid colors without spelling mistakes or repeats for each turn.'
+      end
+    end
+    puts "You guessed: #{@chosen_colors}"
+    @turns += 1
+  end
+end
+
+class Computer < Guesser
+  def initialize
+    super
+    puts 'beepboop'
+  end
+
+  def make_guess
+    sleep 1
+    @chosen_colors = []
+    puts 'Okay, computer.. Guess one at a a time. Start with 1, then 2, etc.'
+    @chosen_colors = Game.pick_sequence
+    puts "Computer: I guess #{@chosen_colors}."
+    @turns += 1
+  end
 end
 
 class Game
@@ -44,11 +59,15 @@ class Game
   @@color_options = %w[red orange yellow green blue purple]
 
   def initialize
-    @correct_sequence = pick_sequence
+    @correct_sequence = Game.pick_sequence
     @game_active = true
     p "For debugging only: #{@correct_sequence}"
     start_message
     start_game
+  end
+
+  def self.color_options
+    @@color_options
   end
 
   def start_message
@@ -58,35 +77,39 @@ class Game
     if answer.downcase == 'g'
       puts 'Okay, you are the guesser'
       create_player
+    elsif answer.downcase == 'c'
+      puts 'Alright, you are the creator.'
+      create_computer
     end
     puts 'Choose four colors. The options are red, orange, yellow, green, blue, and purple.'
   end
 
   def create_player
-    @player = Player.new
+    @guesser = Player.new
   end
 
-  def self.color_options
-    @@color_options
+  def create_computer
+    @guesser = Computer.new
   end
 
-  def pick_sequence
-    @@color_options[0..3].shuffle
+  def self.pick_sequence
+    shuffled = @@color_options.shuffle
+    shuffled[0..3]
   end
 
   def start_game
     while @game_active
-      @player.make_guess
+      @guesser.make_guess
       give_feedback
       check_game_state
     end
   end
 
   def check_game_state
-    if @player.turns == 12 && game_active
+    if @guesser.turns == 12 && game_active
       puts "Out of turns. The answer was: #{correct_sequence}."
       @game_active = false
-    elsif @player.chosen_colors == @correct_sequence
+    elsif @guesser.chosen_colors == @correct_sequence
       puts 'Winner!'
       @game_active = false
     end
@@ -95,10 +118,10 @@ class Game
   def give_feedback
     correct = 0
     right_color = 0
-    @player.chosen_colors.each_index do |index|
-      if @player.chosen_colors[index] == correct_sequence[index]
+    @guesser.chosen_colors.each_index do |index|
+      if @guesser.chosen_colors[index] == correct_sequence[index]
         correct += 1
-      elsif correct_sequence.include?(@player.chosen_colors[index]) && @player.chosen_colors[index] != correct_sequence[index]
+      elsif correct_sequence.include?(@guesser.chosen_colors[index]) && @guesser.chosen_colors[index] != correct_sequence[index]
         right_color += 1
       end
     end
