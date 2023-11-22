b70505501d065b88295d5c4d534a7ea9fb882dc5
Christian Shaffer <calexandershaffer@gmail.com>
2023-11-22 11:11:00 -0500

    Fix turn counting bug
    Changed check_game_state function bug to allow correct guess on the 12th
    turn instead of ending after 12 regardless of the input.

--- app.rb
+++ app.rb
@@ -1,58 +1,3 @@
-class Guesser
-  attr_accessor :chosen_colors, :turns
-
-  def initialize
-    @chosen_colors = []
-    @turns = 0
-  end
-
-  def valid_guess?(guess)
-    if !@chosen_colors.include?(guess) && Game.color_options.include?(guess)
-      true
-    else
-      false
-    end
-  end
-end
-
-class Player < Guesser
-  def initialize
-    super
-    puts 'hehe'
-  end
-
-  def make_guess
-    @chosen_colors = []
-    puts 'Guess one at a a time. Start with 1, then 2, etc.'
-    while @chosen_colors.length < 4
-      guess = gets.chomp
-      if valid_guess?(guess)
-        @chosen_colors << guess
-      else
-        puts 'Invalid. Needs to be one of the six valid colors without spelling mistakes or repeats for each turn.'
-      end
-    end
-    puts "You guessed: #{@chosen_colors}"
-    @turns += 1
-  end
-end
-
-class Computer < Guesser
-  def initialize
-    super
-    puts 'beepboop'
-  end
-
-  def make_guess
-    sleep 1
-    @chosen_colors = []
-    puts 'Okay, computer.. Guess one at a a time. Start with 1, then 2, etc.'
-    @chosen_colors = Game.pick_sequence
-    puts "Computer: I guess #{@chosen_colors}."
-    @turns += 1
-  end
-end
-
 class Game
   attr_accessor :correct_sequence, :turns, :game_active
 
@@ -106,11 +51,11 @@ class Game
   end
 
   def check_game_state
-    if @guesser.turns == 12 && game_active
-      puts "Out of turns. The answer was: #{correct_sequence}."
-      @game_active = false
-    elsif @guesser.chosen_colors == @correct_sequence
+    if @guesser.chosen_colors == @correct_sequence
       puts 'Winner!'
+      @game_active = false
+    elsif @guesser.turns == 12 && game_active
+      puts "Out of turns. The answer was: #{@correct_sequence}."
       @game_active = false
     end
   end
@@ -127,6 +72,61 @@ class Game
     end
     puts "Correct: #{correct}"
     puts "Right color: #{right_color}"
+  end
+end
+
+class Guesser
+  attr_accessor :chosen_colors, :turns
+
+  def initialize
+    @chosen_colors = []
+    @turns = 0
+  end
+
+  def valid_guess?(guess)
+    if !@chosen_colors.include?(guess) && Game.color_options.include?(guess)
+      true
+    else
+      false
+    end
+  end
+end
+
+class Player < Guesser
+  def initialize
+    super
+    puts 'hehe'
+  end
+
+  def make_guess
+    @chosen_colors = []
+    puts 'Guess one at a a time. Start with 1, then 2, etc.'
+    while @chosen_colors.length < 4
+      guess = gets.chomp
+      if valid_guess?(guess)
+        @chosen_colors << guess
+      else
+        puts 'Invalid. Needs to be one of the six valid colors without spelling mistakes or repeats for each turn.'
+      end
+    end
+    puts "You guessed: #{@chosen_colors}"
+    @turns += 1
+  end
+end
+
+class Computer < Guesser
+  def initialize
+    super
+    puts 'beepboop'
+  end
+
+  def make_guess
+    #sleep 1
+    puts 'Okay, computer.. Guess one at a a time. Start with 1, then 2, etc.'
+    @chosen_colors = Game.pick_sequence
+    puts "Computer: I guess #{@chosen_colors}."
+    @turns += 1
+    puts @turns
   end
 end
 
